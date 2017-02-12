f5bde2f4f5e982e45a6deef34b700f5b71059a46
Carlos Aguilera <ceaf.21@gmail.com>
2017-02-12 19:07:51 -0400

    %modificaciones en gulpfile para inyectar archivos js y css propios

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@
 
 var gulp        = require('gulp');
 var wiredep     = require('wiredep').stream;
+var sass        = require('gulp-sass');
+var inject      = require('gulp-inject');
 
-gulp.task('wiredep', function (){
+
+gulp.task('wiredep', () => {
     gulp.src('./src/Guikifix/PresentationBundle/app/index.html')
     .pipe(wiredep({
         directory: './bower_components',
@@ -14,4 +17,21 @@ gulp.task('wiredep', function (){
     .pipe(gulp.dest('./src/Guikifix/PresentationBundle/app'))
 });
 
-gulp.task('default', ['wiredep']);
+gulp.task('sass', () => {
+    console.log("ejecuto sass");
+    gulp.src('./src/Guikifix/PresentationBundle/app/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/Guikifix/PresentationBundle/app/'))
+})
+
+gulp.task('inject', ['sass'], () => {
+  console.log("ejecuto inject");
+  var target = gulp.src('./src/Guikifix/PresentationBundle/app/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths: 
+  var sources = gulp.src(['./src/Guikifix/PresentationBundle/app/**/*.js', './src/Guikifix/PresentationBundle/app/**/*.css'], {read: false});
+ 
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./src/Guikifix/PresentationBundle/app/'));
+});
+
+gulp.task('default', ['wiredep', 'sass', 'inject']);
