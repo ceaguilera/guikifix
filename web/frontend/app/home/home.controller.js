c6be36d6bde84ef45dc4452ad48141536966adff
root <root@developer>
2017-02-24 02:58:09 -0400

    +agregado listado de pruebas y iconos referenciales de la barra de abajo

--- web/frontend/app/home/home.controller.js
+++ web/frontend/app/home/home.controller.js
@@ -1,4 +1,19 @@
 angular.module('guikifixApp.home')
 .controller('home', function($scope, $log) {
     $scope.saludo = "holaaaaa";
+    $scope.specialtiesList = [
+        {
+            id: 123,
+            name: 'PLOMERIA'
+        },
+        {
+            id: 124,
+            name: 'CARPINTERIA'
+        },
+        {
+            id: 125,
+            name: 'ALBAÑILERIA'
+        }
+    ]
+    console.log($scope.specialtiesList);
 });
