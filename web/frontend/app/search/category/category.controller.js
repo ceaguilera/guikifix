59b70d0df1ff05a97165ba022f2f65c21fb1397a
root <root@developer>
2017-03-10 00:25:44 -0400

    %modificado comportamineto para guardar estructura para mandar a backend

--- web/frontend/app/search/category/category.controller.js
+++ web/frontend/app/search/category/category.controller.js
@@ -2,7 +2,6 @@ angular.module('guikifixApp.category')
 .controller('category', function($scope, $log, $http, $stateParams,$rootScope, $location) {
     
     $scope.selected = parseInt($stateParams.categoryId);
-    
     const init = (selected) => {
         let url = "/api/jobtype/"+selected;
         let updateURL = "/category/"+selected;
@@ -12,6 +11,8 @@ angular.module('guikifixApp.category')
             url : url
         }).then(function mySucces(response) {
             $scope.categories = response.data;
+            $scope.arraySelection = new Array($scope.categories.length);
+            console.log($scope.arraySelection);
             console.log($scope.categories);
         }, function myError(response) {
             console.log(response.statusText);
@@ -19,8 +20,19 @@ angular.module('guikifixApp.category')
     }
     init($scope.selected);
 
+    /* Inicializa el primer select cuando cambia */
     $scope.categoryChange = (selected) => {
         init(selected);
+    }
+
+    /* Inicializa la posicion del array principal que es un check */
+    $scope.initArray = (key) => {
+        $scope.arraySelection[key] = new Array($scope.categories[key].children.length);
+    }
+
+    /* Cambia el estado del check en el array de check en el array pricipal */
+    $scope.changeCheck = (key, keySub, id, check) => {
+        $scope.arraySelection[key][keySub] = check ? id : null;
     }
 
 });
