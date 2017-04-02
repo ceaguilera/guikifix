e0df3a0883d7fe7b048f5800d966128ea243b0f0
root <root@developer>
2017-04-01 20:45:44 -0400

    Pruebas del login

--- web/frontend/app/app.module.js
+++ web/frontend/app/app.module.js
@@ -3,7 +3,8 @@
   angular
     .module('guikifixApp', 
     [
-        'ui.router', 
+        'ui.router',
+        'ngCookies',
         'guikifixApp.home', 
         'guikifixApp.about', 
         'guikifixApp.login',
@@ -21,4 +22,72 @@ angular.module('guikifixApp')
           requireBase: false
       });
 }]);
+
+//factoria que controla la autentificación, devuelve un objeto
+//$cookies para crear cookies
+//$cookieStore para actualizar o eliminar
+//$location para cargar otras rutas
+angular.module('guikifixApp').factory("auth", function($cookies,$cookieStore,$location, $http)
+{
+    return{
+        login : function(email, password)
+        {
+            let data = {
+                userName: email,
+                pass : password
+            }
+            $http({
+                method : "POST",
+                url : "/api/user/login",
+                data : data,
+            }).then(function mySucces(response) {
+                console.log(response);
+            }, function myError(response) {
+                console.log(response);
+            })
+            //console.log(email, password);
+            //var error = "error en el email";
+            //return error;
+            //creamos la cookie con el nombre que nos han pasado
+            //$cookies.username = username,
+            //$cookies.password = password;
+            //mandamos a la home
+            //$location.path("/home");
+        },
+        logout : function()
+        {
+            //al hacer logout eliminamos la cookie con $cookieStore.remove
+            $cookieStore.remove("username"),
+            $cookieStore.remove("password");
+            //mandamos al login
+            $location.path("/login");
+        },
+        checkStatus : function()
+        {
+            //creamos un array con las rutas que queremos controlar
+            var rutasPrivadas = ["/home","/dashboard","/login"];
+            if(this.in_array($location.path(),rutasPrivadas) && typeof($cookies.username) == "undefined")
+            {
+                $location.path("/login");
+            }
+            //en el caso de que intente acceder al login y ya haya iniciado sesión lo mandamos a la home
+            if(this.in_array("/login",rutasPrivadas) && typeof($cookies.username) != "undefined")
+            {
+                $location.path("/home");
+            }
+        },
+        in_array : function(needle, haystack)
+        {
+            var key = '';
+            for(key in haystack)
+            {
+                if(haystack[key] == needle)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+});
         
--- web/frontend/app/login/login.controller.js
+++ web/frontend/app/login/login.controller.js
@@ -1,5 +1,9 @@
 angular.module('guikifixApp.login')
-.controller('login', function($scope, $log, $http) {
-    $scope.saludo = "holaaaaa";
-    console.log("caro el controlador");
+.controller('login', function($scope, $log, $http, auth) {
+    //la función login que llamamos en la vista llama a la función
+    //login de la factoria auth pasando lo que contiene el campo
+    //de texto del formulario
+    $scope.login = () =>  {
+        auth.login($scope.email, $scope.password);
+    }
 });
