faeb9a7e1fe7a914c9100e5b12a4a92cc221c334
fcontreras <freddycontreras3@gmail.com>
2017-04-17 21:44:44 -0400

    + Integrando el correo de registro userc7.

--- src/Guikifix/Core/Contract/HandlerBase.php
+++ src/Guikifix/Core/Contract/HandlerBase.php
@@ -11,13 +11,25 @@ namespace Guikifix\Core\Contract;
  */
 class HandlerBase
 {
+	protected $container;
+
     protected $services = [
         'repositoryFactory' => 
             'Guikifix\ApiBundle\Services\RepositoryFactory'
     ];
 
+    public function __construct($container)
+    {
+    	$this->container = $container;
+    }
+
     public function get($service)
     {
         return new $this->services[$service];
+    }
+
+    public function setContainer($container)
+    {
+    	$this->container = $container;
     }
 }
