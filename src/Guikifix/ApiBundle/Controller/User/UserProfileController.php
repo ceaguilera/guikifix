a9bb03420964bc59935f7964c405ea3ef2fc9d82
Joel David <lotus@lotus>
2017-04-23 16:55:56 -0400

    % Creando la ruta para el guardar la informacion del usuario.

--- src/Guikifix/ApiBundle/Controller/User/UserProfileController.php
+++ src/Guikifix/ApiBundle/Controller/User/UserProfileController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Guikifix\Core\UseCases\User\Security\UserProfile\GetInfo\GetInfoCommand;
+use Guikifix\Core\UseCases\User\Security\UserProfile\SetInfo\SetInfoCommand;
 
 
 class UserProfileController extends Controller
@@ -36,6 +37,88 @@ class UserProfileController extends Controller
             return new JsonResponse(false, 302);
             
         $command = new GetInfoCommand($data);
+        $response = $this->get('CommandBus')->execute($command);
+
+        return new JsonResponse($response->getData(),$response->getStatusCode());
+    }
+
+    /**
+     * Esta función es usada para guardar la informacion del perfil del usuario.
+     *
+     * @param  Request $request 
+     * @return json    data solicitada
+     * @ApiDoc(
+     *     resource=true,
+     *     views={"default","job"},
+     *     parameters={
+     *         {
+     *              "name"="first_name",
+     *              "dataType"="string",
+     *              "description"="Nombre del usuario",
+     *              "required"="true"
+     *         },
+     *         {
+     *              "name"="last_name",
+     *              "dataType"="string",
+     *              "description"="Apellido del usuario",
+     *              "required"="true"
+     *         },
+     *         {
+     *              "name"="identity_card",
+     *              "dataType"="string",
+     *              "description"="Cedula del usuario",
+     *              "required"="true"
+     *         },
+     *         {
+     *              "name"="cell_phone",
+     *              "dataType"="string",
+     *              "description"="Celular del usuario",
+     *              "required"="true"
+     *         },
+     *         {
+     *              "name"="phone",
+     *              "dataType"="string",
+     *              "description"="Telefono del usuario",
+     *              "required"="false"
+     *         },
+     *         {
+     *              "name"="gender",
+     *              "dataType"="string",
+     *              "description"="Genero del usuario",
+     *              "required"="true"
+     *         },
+     *         {
+     *              "name"="birthdate",
+     *              "dataType"="string",
+     *              "description"="Fecha de cumpleaños del usuario",
+     *              "required"="true"
+     *         },
+     *         {
+     *              "name"="city",
+     *              "dataType"="string",
+     *              "description"="Id de la parroquia del usuario",
+     *              "required"="true"
+     *         }
+     *         
+     *     },
+     *     resourceDescription="Url para guardar la informacion del perfil del usuario.",
+     *     description="Url para guardar la informacion del perfil del usuario.",
+     *      statusCodes={
+     *         200="True",
+     *         302="No Logged",
+     *         500="Error en el servidor"
+     *     }
+     *  )
+     */
+    public function setInfoAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        $data["user"] = $this->get('security.token_storage')->getToken()->getUser();
+
+        if (!is_object($data["user"]))
+            return new JsonResponse(false, 302);
+            
+        $command = new SetInfoCommand($data);
         $response = $this->get('CommandBus')->execute($command);
 
         return new JsonResponse($response->getData(),$response->getStatusCode());
