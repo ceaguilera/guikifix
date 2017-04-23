c2e8e40a1ad15e587368c0f396e6203e3bb0da35
Joel David <lotus@lotus>
2017-04-23 14:33:16 -0400

    % Creando la migracion para el manejo de identify_card y photo_profile

---
+++ app/DoctrineMigrations/Version20170423183257.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170423183257 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE user_profile ADD photo_profile VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user_profile ADD identity_card VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE user_profile DROP photo_profile');
+        $this->addSql('ALTER TABLE user_profile DROP identity_card');
+    }
+}
