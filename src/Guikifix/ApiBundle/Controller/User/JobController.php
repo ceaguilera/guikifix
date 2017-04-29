fd5e6167b1adc0e332f5d34139ada567a7e4bf34
jrequena <joel.2005.2@gmail.com>
2017-04-29 14:07:51 -0400

    +Manejo de presupuesto con email.

--- src/Guikifix/ApiBundle/Controller/User/JobController.php
+++ src/Guikifix/ApiBundle/Controller/User/JobController.php
@@ -61,8 +61,15 @@ class JobController extends Controller
         $data = json_decode($request->getContent(), true);
         $data["user"] = $this->get('security.token_storage')->getToken()->getUser();
 
-        if (!is_object($data["user"]))
-            return new JsonResponse(false, 302);
+        if (!is_object($data["user"])){
+            $email = isset($data["email"]) ? $data["email"] : null;
+            if (is_null($email)) 
+                return new JsonResponse(false, 302);
+            $user = $this->get('repositoryFactory')->get('User')->findBy(["email"=>$email]);
+            if (!$user) 
+                return new JsonResponse(false, 302);
+            $data["user"] = $user;
+        }
             
         $command = new SetJobCommand($data);
         $response = $this->get('CommandBus')->execute($command);
